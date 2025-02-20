a9bc180286e4436e2a043f5ef7f686293d8f8627
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-02-20 12:52:07 +0530

    add product

--- routes/api.php
+++ routes/api.php
@@ -54,7 +54,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::prefix('products')->group(function () {
             Route::post('/get_products/{id?}', [ProductController::class, 'index']);          // List all products
             Route::get('/{slug}', [ProductController::class, 'show']);     // Get details of a single product
-            Route::post('/', [ProductController::class, 'store']);         // Add a new product (Admin only)
+            Route::post('/register', [ProductController::class, 'store']);         // Add a new product (Admin only)
             Route::put('/{id}', [ProductController::class, 'update']);     // Update a product (Admin only)
             Route::delete('/{id}', [ProductController::class, 'destroy']); // Delete a product (Admin only)
             Route::post('/import', [ProductController::class, 'importProductsFromCsv']);
