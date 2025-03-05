eef30c6c34dd285e35d6808082ce9ba5a15ea8c8
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-03-05 15:46:08 +0530

    add relations

--- app/Models/QuotationModel.php
+++ app/Models/QuotationModel.php
@@ -10,4 +10,24 @@ class QuotationModel extends Model
     protected $table = 't_quotations';
 
     protected $fillable = ['user_id', 'total_amount', 'status', 'payment_status', 'shipping_address', 'razorpay_order_id'];
+
+    /**
+     * Relation to the User table
+     * Foreign Key: user_id (on Order table)
+     * Primary Key: id (on User table)
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id'); // user_id references id in User table
+    }
+
+     /**
+     * Relation to the OrderItem table
+     * Foreign Key: order_id (on OrderItem table)
+     * Primary Key: id (on Order table)
+     */
+    public function items()
+    {
+        return $this->hasMany(QuotationItemModel::class, 'quotation_id', 'id'); // order_id references id in Order table
+    }
 }
