db854392c0a778d747c689e9cb87e7576be7f1e5
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-03-22 14:43:00 +0530

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,7 @@ use App\Models\BrandModel;
 use App\Models\CategoryModel;
 use App\Models\ProductModel;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 //use Illuminate\Support\Facades\Cookie;
 
 class UserController extends Controller
