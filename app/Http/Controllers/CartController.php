a63c077da371440bcfe30f8ed6cdc00d8c7eb57f
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-08-07 16:16:57 +0530

    test

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -135,6 +135,8 @@ class CartController extends Controller
             $cartItem->variant->makeHidden(['created_at', 'updated_at']);
         }
         // Calculate the selling price based on user role
+
+        dd($user->role);
         if ($user && $user->role == 'admin') {
             // Admin: Use regular price as the selling price
             $cartItem->selling_price = $cartItem->product->regular_price;
