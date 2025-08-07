f70b92870bf1e1d6511743d4f990f5bcd6040cef
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-08-07 17:37:48 +0530

    update

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -176,9 +176,10 @@ class CartController extends Controller
                 'id' => $cartItem->id,
                 'user_name' => $cartItem->user->name,
                 'product_name' => $cartItem->product->name,
-                'variant_value' => $cartItem->variant_value,
+                'variant_value' => $variantType,
                 'selling_price' => $cartItem->selling_price,
                 'quantity' => $cartItem->quantity,
+                'file_urls' => $fileUrls,
             ];
         });
 
