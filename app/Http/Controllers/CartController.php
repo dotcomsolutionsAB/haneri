f084a06ef7327eebc6f5dd5a73657aa38d6ba120
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-08-07 16:53:08 +0530

    fix

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\CartModel;
+use App\Models\UsersDiscountModel;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\PersonalAccessToken;
 use Str;
