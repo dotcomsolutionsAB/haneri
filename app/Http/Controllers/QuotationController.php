9d2171b50e06defc678d6083730417207b72928c
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-08-08 12:50:03 +0530

    test

--- app/Http/Controllers/QuotationController.php
+++ app/Http/Controllers/QuotationController.php
@@ -166,7 +166,7 @@ class QuotationController extends Controller
 
                 // Apply the discount (calculate price after discount)
                 $discountedPrice = number_format($regularPrice - ($regularPrice * ($discount / 100)), 0);
-                dd($discountedPrice);
+                dd(floatval($discountedPrice));
                 return max(0, floatval($discountedPrice)); // Ensure price doesn't go below 0
             }
             return 0; // Return 0 if variant not found
