2420948b478233fd35b44da2e598924aeca13313
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-08-08 16:17:15 +0530

    test

--- resources/views/quotation_invoice_template_items.php
+++ resources/views/quotation_invoice_template_items.php
@@ -2,23 +2,10 @@
     <td class="center-align">{{ $index + 1 }}</td>
     <td><img src="{{ Storage::url('uploads/products_pdf/' . $item->product_code . '.jpg') }}" alt="Product Image" style="height: 60px; width: 60px;"></td>
     <td>
-        print_r($item);
     {{ $item->product_name }}<br>
-    Part No: {{ $item->product->product_code }}<br>
-
-    @if(! empty($item->size))
-        <span style="background: yellow;">
-            Size: {{ $item->size }}
-        </span><br>
-    @endif
-
-    @if(! empty($item->remarks))
-        <span style="background: yellow;">
-            {{ $item->remarks }}
-        </span><br>
-    @endif
+    Part No: {{ $item->product_id }}<br>
 </td>
     <td class="center-align">{{ $item->quantity }}</td>
     <td class="right-align">₹ {{ number_format((float)$item->rate, 2) }}</td>
-    <td class="right-align">₹ {{ number_format((float)$item->total, 2) }}</td>
+    <td class="right-align">₹ {{ number_format((float)$item->price, 2) }}</td>
 </tr>
