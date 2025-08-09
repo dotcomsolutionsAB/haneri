149e4868d80bd25c663b94be3e2b8d98a7b77c73
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-08-09 15:02:57 +0530

    fix fillable

--- app/Models/QuotationModel.php
+++ app/Models/QuotationModel.php
@@ -9,7 +9,7 @@ class QuotationModel extends Model
     //
     protected $table = 't_quotations';
 
-    protected $fillable = ['user_id', 'total_amount', 'q_user', 'q_email', 'q_mobile', 'q_address'];
+    protected $fillable = ['user_id', 'total_amount', 'q_user', 'q_email', 'q_mobile', 'q_address', 'invoice_quotation'];
 
     /**
      * Relation to the User table
