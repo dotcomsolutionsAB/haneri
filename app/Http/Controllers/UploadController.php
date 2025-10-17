70c5ad80ca064a117c814cd000bf6efa7d1f8209
Shaqlin <shaqlinmondal8905@gmail.com>
2025-10-17 14:51:23 +0530

    ..

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Models\ProductVariantModel;
+use App\Models\ProductModel;
 use App\Models\UploadModel;
+
 
 class UploadController extends Controller
 {
