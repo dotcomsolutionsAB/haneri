fdad5f68d69dd645bffee1c936f975e2d4993045
Shaqlin <shaqlinmondal8905@gmail.com>
2025-11-28 23:26:15 +0530

    ..

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Services\GoogleAuthService;
+use App\services\GoogleAuthService;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
