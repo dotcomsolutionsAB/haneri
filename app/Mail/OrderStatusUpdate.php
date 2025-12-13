4c2596bd54e13e425da2441f61c2ab9c31260c04
Shaqlin <shaqlinmondal8905@gmail.com>
2025-12-13 14:36:00 +0530

    ..

--- app/Mail/OrderStatusUpdate.php
+++ app/Mail/OrderStatusUpdate.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use App\Models\UploadModel;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
